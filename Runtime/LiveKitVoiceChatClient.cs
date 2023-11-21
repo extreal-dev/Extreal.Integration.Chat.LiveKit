00f9ea929822fdd57fac06cf18f6f2286aa3160e
sus-yoshikane-t <yoshikane-t@m.sus-g.co.jp>
2023-11-21 16:22:59 +0900

    Add public method templates

--- Runtime/LiveKitVoiceChatClient.cs
+++ Runtime/LiveKitVoiceChatClient.cs
@@ -1,16 +1,50 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using System;
+using Cysharp.Threading.Tasks;
+using Extreal.Core.Common.System;
+using UniRx;
 
 namespace Extreal.Integration.Chat.LiveKit
 {
-    public class LiveKitVoiceChatClient
+    public class LiveKitVoiceChatClient : DisposableBase
     {
-        public void StartClient(string endpoint, string roomName, string playerName)
+        public IObservable<bool> OnMuted => onMuted;
+        private Subject<bool> onMuted = new Subject<bool>();
+
+        public LiveKitVoiceChatClient()
         {
+
+        }
+
+        public async UniTask StartClient(string accessTokenEndpoint, string livekitEndpoint, string roomName, string playerName)
+        {
+
         }
 
         public async UniTask StopClient()
         {
 
+        }
+
+        public async UniTask ToggleMute()
+        {
+
+        }
+
+        public async UniTask SetVolume(float value)
+        {
+
+        }
+
+        public async UniTask SetDevice(int deviceId)
+        {
+
+        }
+
+        protected override void ReleaseManagedResources()
+        {
+            onMuted.Dispose();
+
+            base.ReleaseManagedResources();
         }
     }
 }
